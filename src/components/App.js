926c9a4b27c7a68578df11ae9a8360019736373b
Natasha <natashakhema@gmail.com>
2019-01-05 13:44:01 +1100

    image fetch

--- src/components/App.js
+++ src/components/App.js
@@ -5,20 +5,25 @@ import SearchBar from "./SearchBar";
 
 
 class App extends React.Component {
-    onSearchSubmit(term) {
+    state = {images: [] }; //state default is empty array because we are expecting an array of images
+    
+    onSearchSubmit = async (term) => {
         //using AXIOS to make network request to Unsplash API
-        axios.get("https://api.unsplash.com/search/photos", {
+        const response = await axios.get("https://api.unsplash.com/search/photos", {
             params: {query: term},
             headers: {
                 Authorization: "Client-ID 96dfe8eecd39c471b1e47949aeb4731b59ffe7330c4444d584956e19b5cb5081"
             }
+        // }).then(response => { //working with promises use .then  method
         });
+        this.setState({ images: response.data.results});
     }
 
     render () {
         return (
             <div className="ui container" style={{marginTop: "10px"}}>
                 <SearchBar onSubmit={this.onSearchSubmit} />
+                Found: {this.state.images.length} images
             </div>
         );
     }
